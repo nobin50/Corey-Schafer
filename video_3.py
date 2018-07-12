a56b54233ead51ad9fd613bfd9e01df71f639302
nobin50 <nobin.mbstu@gmail.com>
2018-07-13 02:20:07 +0600

    Youtube video number (3)

---
+++ video_3.py
@@ -0,0 +1,48 @@
+num1 = 3
+num2 = 3.14
+print(type(num1))
+print(type(num2))
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(2 % 2)
+print(3 % 2)
+print(4 % 2)
+print(5 % 2)
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+
+num = 1
+num = num + 1
+print(num)
+num += 10
+print(num)
+num *= 10
+print(num)
+
+print(abs(-3))
+print(round(3.14))
+print(round(3.75))
+print(round(3.75, 1))
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+
+num1 = '100'
+num2 = '200'
+
+print(num1+num2)
+print(int(num1) + int(num2))
+
